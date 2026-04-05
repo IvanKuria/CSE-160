ffca8b3317ba3548c0f94f1e7d66dfe99625554f
Ivan Kuria <ivankuria7@gmail.com>
2026-04-05 16:37:46 -0700

    up to part 3

---
+++ asgn0/asgn0.js
@@ -0,0 +1,48 @@
+// DrawTriangle.js (c) 2012 matsuda
+function main() {  
+  // Retrieve <canvas> element
+  canvas = document.getElementById('example');  
+  if (!canvas) { 
+    console.log('Failed to retrieve the <canvas> element');
+    return false; 
+  } 
+
+  // Get the rendering context for 2DCG
+  ctx = canvas.getContext('2d'); // decided to make it a global var
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  var v1 = new Vector3([2.25, 2.25, 0]);
+  drawVector(v1, "red");
+}
+
+function drawVector(v, color) {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.moveTo(200, 200);
+  ctx.lineTo(200 + (v.elements[0] * 20), 200 - (v.elements[1] * 20));
+  ctx.stroke();
+}
+
+function handleDrawEvent() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clears canvas
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+    const x = document.getElementById("x").value; // x val
+    const y = document.getElementById("y").value; // y val
+
+    const x2 = document.getElementById("x-2").value; // x-2 val
+    const y2 = document.getElementById("y-2").value; // y-2 val 
+    
+    var v1 = new Vector3([x, y, 0]);
+    var v2 = new Vector3([x2, y2, 0]);
+    drawVector(v1, "red")
+    drawVector(v2, "blue")
+}
+
+// main();
+
+// sources: 
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath
